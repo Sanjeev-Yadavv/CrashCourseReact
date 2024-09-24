13790ea1b17435cd2e89c0b73f239a03b4741ded
sanjeev-yadavv <kumarsanjeev.sk575@gmail.com>
2024-09-24 14:49:02 +0530

    settin routes using useNavigate

--- routing/set-basicroutes/src/App.jsx
+++ routing/set-basicroutes/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -6,10 +6,16 @@ import Product from './components/Product'
 import './App.css'
 
 function App() {
-  
+  const navigate = useNavigate()
 
   return (
     <>
+    <div className='navbar'>
+      <button onClick={()=> navigate('/')}>Home</button>
+      <button onClick={()=> navigate('/contact')}>Contact</button>
+      <button onClick={()=> navigate('/about')}>About</button>
+      <button onClick={()=> navigate('/product')}>Product</button>
+    </div>
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
